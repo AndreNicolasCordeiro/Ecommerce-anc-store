358370ebbb60a978bc9a91c00413da1ee189aa1f
André Nicolas Cordeiro <andrenicolascordeiro@gmail.com>
2023-10-23 20:39:18 -0300

    feat: add HOST_URL

--- env.d.ts
+++ env.d.ts
@@ -6,5 +6,6 @@ declare namespace NodeJS {
         STRIPE_SECRET_KEY: string;
         NEXT_PUBLIC_STRIPE_PUBLIC_KEY: string;
         STRIPE_WEBHOOK_SECRET_KEY: string;
+        HOST_URL: string;
     }
 }
