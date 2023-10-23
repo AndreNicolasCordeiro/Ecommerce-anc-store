2aaee2ebfd09fb4417d8d750c46fc3b0c1f7e80a
André Nicolas Cordeiro <andrenicolascordeiro@gmail.com>
2023-10-23 20:48:58 -0300

    feat: add deals page

---
+++ src/app/deals/page.tsx
@@ -0,0 +1,37 @@
+import { Badge } from "@/components/ui/badge";
+import ProductItem from "@/components/ui/product-item";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { prismaClient } from "@/lib/prisma";
+import { PercentIcon } from "lucide-react";
+
+const DealsPage = async () => {
+  const deals = await prismaClient.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
+
+  return (
+    <div className="flex flex-col gap-8 p-5">
+      <Badge
+        className="w-fit gap-2 border-2 border-primary px-3 py-[0.365rem] text-base font-bold uppercase"
+        variant="outline"
+      >
+        <PercentIcon size={16} />
+        Ofertas
+      </Badge>
+      <div className="gri-cols-2 grid gap-8">
+        {deals.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={computeProductTotalPrice(product)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default DealsPage;
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -103,10 +103,17 @@ const Header = () => {
               </Link>
             </SheetClose>
 
-            <Button variant="outline" className="w-full justify-start gap-2">
-              <PercentCircle size={16} />
-              Ofertas
-            </Button>
+            <SheetClose asChild>
+              <Link href="/deals">
+                <Button
+                  variant="outline"
+                  className="w-full justify-start gap-2"
+                >
+                  <PercentCircle size={16} />
+                  Ofertas
+                </Button>
+              </Link>
+            </SheetClose>
 
             <SheetClose asChild>
               <Link href="/catalog">
