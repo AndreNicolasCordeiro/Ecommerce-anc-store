029fecf148c310b912862daa7c84c4a2f444de98
André Nicolas Cordeiro <andrenicolascordeiro@gmail.com>
2023-10-23 20:13:16 -0300

    chore: ajustament localStorage

--- src/providers/cart.tsx
+++ src/providers/cart.tsx
@@ -36,9 +36,13 @@ export const CartContext = createContext<ICartContext>({
 });
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
-  const [products, setProducts] = useState<CartProduct[]>(
-    JSON.parse(localStorage.getItem("@anc-store/cart-products") || "[]"),
-  );
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  useEffect(() => {
+    setProducts(
+      JSON.parse(localStorage.getItem("@anc-store/cart-products") || "[]"),
+    );
+  }, []);
 
   useEffect(() => {
     localStorage.setItem("@anc-store/cart-products", JSON.stringify(products));
