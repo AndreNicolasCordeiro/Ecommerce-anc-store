ca4ea16305782df25904e275c55e2cf81b1198c9
André Nicolas Cordeiro <andrenicolascordeiro@gmail.com>
2023-10-23 22:17:40 -0300

    feat: add createOrder server action

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -1,8 +1,10 @@
 import { createCheckout } from "@/actions/checkout";
+import { createOrder } from "@/actions/order";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { CartContext } from "@/providers/cart";
 import { loadStripe } from "@stripe/stripe-js";
 import { ShoppingCartIcon } from "lucide-react";
+import { useSession } from "next-auth/react";
 import { useContext } from "react";
 import { Badge } from "./badge";
 import { Button } from "./button";
@@ -11,21 +13,27 @@ import { ScrollArea } from "./scroll-area";
 import { Separator } from "./separator";
 
 const Cart = () => {
+  const { data } = useSession();
+
   const { products, totalDiscount, total, subTotal } = useContext(CartContext);
 
   const handleFinishPurchaseClick = async () => {
+    if (!data?.user) {
+      return;
+    }
+
+    await createOrder(products, (data?.user as any).id);
+
     const checkout = await createCheckout(products);
 
-    const stripe = await loadStripe(
-      process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY
-    )
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
     stripe?.redirectToCheckout({
       sessionId: checkout.id,
-    })
-  }
+    });
+  };
   return (
-    <div className="flex flex-col gap-8 h-full">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-2 border-2 border-primary px-3 py-[0.365rem] text-base font-bold uppercase"
         variant="outline"
@@ -34,56 +42,60 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      <div className="flex flex-col gap-5 h-full overflow-hidden">
-       <ScrollArea className="h-full">
-        <div className="flex h-full flex-col gap-4">
-       {products.length > 0 ? (
-          products.map((product) => (
-            <CartItem
-              key={product.id}
-              product={computeProductTotalPrice(product as any) as any}
-            />
-          ))
-        ) : (
-          <p className="text-center font-semibold">Carrinho vazio :(</p>
-        )}
-        </div>
-       </ScrollArea>
+      <div className="flex h-full flex-col gap-5 overflow-hidden">
+        <ScrollArea className="h-full">
+          <div className="flex h-full flex-col gap-4">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={computeProductTotalPrice(product as any) as any}
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">Carrinho vazio :(</p>
+            )}
+          </div>
+        </ScrollArea>
       </div>
-        
-        {products.length > 0 && (
-          <div className="flex flex-col gap-3">
+
+      {products.length > 0 && (
+        <div className="flex flex-col gap-3">
           <Separator />
           <div className="flex items-center justify-between text-xs">
             <p>Subtotal</p>
             <p>R$ {subTotal.toFixed(2)}</p>
           </div>
-  
+
           <Separator />
-  
+
           <div className="flex items-center justify-between text-xs">
             <p>Entrega</p>
             <p>Grátis</p>
           </div>
-  
+
           <Separator />
-  
+
           <div className="flex items-center justify-between text-xs">
             <p>Descontos</p>
             <p>R$ {totalDiscount.toFixed(2)}</p>
           </div>
-  
+
           <Separator />
-  
+
           <div className="flex items-center justify-between text-sm font-bold">
             <p>Total</p>
             <p>R$ {total.toFixed(2)}</p>
           </div>
-  
-          <Button className="uppercase font-bold mt-7" onClick={handleFinishPurchaseClick}>Finalizar Compra</Button>
+
+          <Button
+            className="mt-7 font-bold uppercase"
+            onClick={handleFinishPurchaseClick}
+          >
+            Finalizar Compra
+          </Button>
         </div>
-        )}
-      
+      )}
     </div>
   );
 };
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -12,6 +12,17 @@ export const authOptions: AuthOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
           })
-        ]
+        ],
+        callbacks: {
+          async session({ session, token, user}) {
+            session.user = { ...session.user, id: user.id } as {
+              id: string;
+              name: string;
+              email: string;
+            }
+
+            return session;
+          }
+        }
       
 }
