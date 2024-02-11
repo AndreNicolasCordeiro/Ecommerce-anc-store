b5555c45b15e44a74a09e99c2d92e724febab68a
André Nicolas Cordeiro <andrenicolascordeiro@gmail.com>
2024-02-11 16:00:20 -0300

    fix: images urls

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -9,33 +9,33 @@ export const dynamic = "force-dynamic"
 
 async function OrderPage() {
 
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    if (!session || !session.user) {
-      return (
-        <div className="flex h-full flex-col items-center justify-center gap-2 p-5">
-          <h2 className="font-bold">Acesso Negado!</h2>
-          <p className="text-sm opacity-60">Faça login para ver seus pedidos</p>
-        </div>
-      );
-    }
-
-    const orders = await prismaClient.order.findMany({
-        where: {
-            userId: session.user.id,
-        },
-        include: {
-            orderProducts: {
-                include: {
-                    product: true,
-                }
-            }
-        }
-    })
-
+  if (!session || !session.user || !('id' in session.user)) {
     return (
-        <div className="p-5">
-            <Badge
+      <div className="flex h-full flex-col items-center justify-center gap-2 p-5">
+        <h2 className="font-bold">Acesso Negado!</h2>
+        <p className="text-sm opacity-60">Faça login para ver seus pedidos</p>
+      </div>
+    );
+  }
+
+  const orders = await prismaClient.order.findMany({
+    where: {
+      userId: session.user.id as string,
+    },
+    include: {
+      orderProducts: {
+        include: {
+          product: true,
+        }
+      }
+    }
+  })
+
+  return (
+    <div className="p-5">
+      <Badge
         className="w-fit gap-2 border-2 border-primary px-3 py-[0.365rem] text-base font-bold uppercase"
         variant="outline"
       >
@@ -44,13 +44,13 @@ async function OrderPage() {
       </Badge>
 
 
-        <div className="flex flex-col gap-5 mt-5">
-      {orders.map(order => (
-        <OrderItem key={order.id} order={order} />
-      ))}
+      <div className="flex flex-col gap-5 mt-5">
+        {orders.map(order => (
+          <OrderItem key={order.id} order={order} />
+        ))}
+      </div>
     </div>
-    </div>
-    )
+  )
 }
 
 export default OrderPage;
